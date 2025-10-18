1cb1918e7cc1dd150edb75a8aa5bc945ec7bdf13
Ayan <sujon.seo.bd@gmail.com>
2025-10-18 19:21:31 +0600

    added stiky site bar

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 
 
@@ -20,6 +22,20 @@ const router = createBrowserRouter([
                 path: "/category/:id",
                 element: <CategoryNews/>,
                 loader: ()=> fetch("../news.json"),
+            },
+        ]
+    },
+    {
+        path: "/auth",
+        element: <h2>Authentication Layout</h2>,
+        children: [
+            {
+                path: "/auth/login",
+                element: <Login/>,
+            },
+            {
+                path: "/auth/register",
+                element: <Register/>
             },
         ]
     },
