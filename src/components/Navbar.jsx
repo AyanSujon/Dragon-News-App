816a061ace5a4abf7b460342341be4d79c9a7982
Ayan <sujon.seo.bd@gmail.com>
2025-10-19 12:21:30 +0600

    added signin function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,20 @@ import { Link, NavLink } from 'react-router';
 import userIcon from "../assets/user.png";
 import { AuthContext } from '../provider/AuthProvider';
 const Navbar = () => {
-    const {user} =use(AuthContext);
+    const {user, logOut} =use(AuthContext);
+
+    const handleLogOut = ()=> {
+        // console.log(" user trying to logout");
+        logOut().then(() => {
+        alert("You Logged Out Successfully!")
+        }).catch((error) => {
+        console.log(error);
+        });
+
+    }
+
+
+
     return (
         <div className='flex justify-between items-center my-5'>
             <div className=''>{user && user.email}</div>
@@ -15,7 +28,12 @@ const Navbar = () => {
             </div>
             <div className='login-btn flex gap-5'>
                 <img src={userIcon} alt="User" />
-                <Link to={"/auth/login"} className='btn btn-primary px-10'>Login</Link>
+                {
+                    user? (
+                        <button onClick={handleLogOut} className='btn btn-primary px-10'>LogOut</button>
+                    ): (<Link to={"/auth/login"} className='btn btn-primary px-10'>Login</Link>
+                )}
+                
             </div>
         </div>
     );
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 
@@ -11,13 +11,25 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
 
-    // console.log(user);
+    console.log(user);
 
+    // Create User Function 
     const createUser = (email, password)=> {
         return(
             createUserWithEmailAndPassword(auth, email, password)
         );
     };
+
+    // LogOut Function
+    const logOut =()=> {
+        return signOut(auth);
+    }
+
+const singnIn = (email, password)=>{
+    return signInWithEmailAndPassword(auth, email, password); 
+};
+
+
 
     useEffect(()=> {
     const unsubscribe =   onAuthStateChanged(auth, (currentUser) =>{
@@ -33,6 +45,8 @@ const AuthProvider = ({children}) => {
         user,
         setUser,
         createUser,
+        logOut,
+        singnIn,
     };
     
 
